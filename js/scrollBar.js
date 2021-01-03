3aee8308236f21e178dc4f352cf04b0d0743b5f5
Javier <ejvquin@gmail.com>
2021-01-03 17:46:48 -0500

    actualizacion 0.1.3

--- js/scrollBar.js
+++ js/scrollBar.js
@@ -1,10 +1,12 @@
 let $ubiPrin = window.pageYOffset; //$ubiPrin = ubicacionPrincipal
-window.onscroll = function(){
+
+ addEventListener("scroll", ()=>{
     let $des = window.pageYOffset; //$des = desplazamiento
     if($ubiPrin >= $des){
         document.getElementById('navbar').style.top = '0';
     }else{
         document.getElementById('navbar').style.top = '-100px';
         document.getElementById("btn-menu").checked = false;
+        //document.querySelector('.submenu').style.
     }
-}
+});
